8112d891f6b50648132752c255f632118cda7603
Tran <tranc27@medtronic.com>
2020-10-23 18:28:57 -0500

    rearrange mapLogic code

--- static/js/map_logic.js
+++ static/js/map_logic.js
@@ -1,9 +1,41 @@
+function createMap(breweryLayer) {
 
-// Create a map object
-var myMap = L.map("map", {
-  center: [44.9778, -93.2650],
-  zoom: 12,
-});
+    // Create the tile layer that will be the background of our map
+    // Add Tile Layer
+    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/streets-v11",
+        accessToken: mapbox_api_key
+    });
+    // Only one base layer can be shown at a time
+    var baseMaps = {
+        "Street Map": streetmap
+    };
+
+    // Overlays that may be toggled on or off
+    var overlayMaps = {
+        "Breweries": breweryLayer
+    };
+
+
+    // Create the map object with options
+    // Create a map object
+    var myMap = L.map("map", {
+        center: [44.9778, -93.2650],
+        zoom: 12,
+        layers: [streetmap, breweryLayer]
+    });
+
+    // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(myMap);
+}
+
+
 
 // Loop Through Breweries and Create Markers
 var breweryMarkers = [];
@@ -18,31 +50,4 @@ for (var i = 0; i < brewData.length; i++) {
     .bindPopup("<h1>" + brewData[i].name + "</h1> <hr> <h2>Rating: " + brewData[i].rating + "</h2> <hr> <h2>No. of Reviews: " + brewData[i].review_count + "</h2>")
   );
 }
-
-// Add all the cityMarkers to a new layer group.
-// Now we can handle them as one group instead of referencing each individually
-var breweryLayer = L.layerGroup(breweryMarkers);
-
-// Add Tile Layer
-var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
-  accessToken: mapbox_api_key
-});
-
-// Only one base layer can be shown at a time
-var baseMaps = {
-  "Street Map": streetmap
-};
-
-// Overlays that may be toggled on or off
-var overlayMaps = {
-  "Breweries": breweryLayer
-};
-
-L.control.layers(baseMaps, overlayMaps, {
-  collapsed: false
-}).addTo(myMap);
+createMap(L.layerGroup(breweryMarkers));
